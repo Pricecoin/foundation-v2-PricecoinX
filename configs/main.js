c1ea49a4324db10c03bd941f87967d9e2055f545
ZachPrice <zach.price105@gmail.com>
2023-12-31 18:43:12 -0600

    push new main.js

--- configs/main.js
+++ configs/main.js
@@ -27,6 +27,84 @@ config.tls = {};
 config.tls.ca = '';
 config.tls.key = '';
 config.tls.cert = '';
+// Banning Configuration
+config.settings.banning = {};
+config.settings.banning.banLength = 600000; // ms;
+config.settings.banning.checkThreshold = 500;
+config.settings.banning.invalidPercent = 50;
+config.settings.banning.purgeInterval = 300000; // ms;
+
+// Timeout Configuration
+config.settings.timeout = {};
+config.settings.timeout.connection = 600000; // ms
+config.settings.timeout.rebroadcast = 60000; // ms
+
+// Interval Configuration
+config.settings.interval = {};
+config.settings.interval.blocks = 1000; // ms
+
+// Shared Configuration
+////////////////////////////////////////////////////////////////////////////////
+
+// Port Configuration
+config.ports = [];
+
+const ports1 = {};
+ports1.port = 3002;
+ports1.enabled = true;
+ports1.tls = false;
+ports1.difficulty = {};
+ports1.difficulty.initial = 32;
+ports1.difficulty.minimum = 8;
+ports1.difficulty.maximum = 512;
+ports1.difficulty.targetTime = 15;
+ports1.difficulty.retargetTime = 90;
+ports1.difficulty.variance = 0.3;
+config.ports.push(ports1);
+
+// Primary Configuration
+////////////////////////////////////////////////////////////////////////////////
+
+// Miscellaneous Configuration
+config.primary = {};
+config.primary.address = '[address]';
+
+// Coin Configuration
+config.primary.coin = {};
+config.primary.coin.name = 'PricecoinX';
+config.primary.coin.symbol = 'PRCX';
+config.primary.coin.algorithm = 'allium';
+
+// Daemon Configuration
+config.primary.daemons = [];
+
+const daemons1 = {};
+daemons1.host = '127.0.0.1';
+daemons1.port = 2332;
+daemons1.username = '';
+daemons1.password = '';
+config.primary.daemons.push(daemons1);
+
+// Recipients Configuration
+config.primary.recipients = [];
+
+const recipient1 = {};
+recipient1.address = '[address]';
+recipient1.percentage = 0.05;
+config.primary.recipients.push(recipient1);
+
+// Blinkhash Donation Address
+const recipient2 = {};
+recipient2.address = 'prc1q2uwz7rsaz2n9wnhs3sdyvjumrcad98ljjuq0w4';
+recipient2.percentage = 0.05;
+config.primary.recipients.push(recipient2);
+
+// ZMQ Configuration
+config.primary.zmq = {};
+config.primary.zmq.enabled = false;
+config.primary.zmq.host = '127.0.0.1';
+config.primary.zmq.port = 29000;
 
 // Export Configuration
 module.exports = config;
+
